1c24532be0ddd2b0e89a5d472e61a1e4a01fe645
Ilya Shestakov <ilya.shestakov.md@gmail.com>
2023-02-26 23:58:59 +0500

    add gh-pages to scripts

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import './index.scss';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from './core/store/store';
 import App from './App';
 
+
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
+
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
